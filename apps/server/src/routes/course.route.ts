13a0d52a8aed5b895ab42d2d692e46c72d9bf6ad
Alejandro <aletomasperren@gmail.com>
2025-08-05 17:31:42 -0300

    add new function Find All Courses

--- apps/server/src/routes/course.route.ts
+++ apps/server/src/routes/course.route.ts
@@ -6,5 +6,6 @@ const controller = courseController();
 
 router.get("/:id", controller.findById);
 router.post("/", controller.createCourse)
+router.get("/", controller.findAllCourses)
 
 export default router
